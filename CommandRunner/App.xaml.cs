f40616226b11ba7a76c0efdd0ff9cf722cbb5f83
Adam Samec <asamec@microsoft.com>
2024-08-22 16:39:14 +0200

    Initial commit for VS solution

---
+++ CommandRunner/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CommandRunner
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            SetLanguageDictionary();
+        }
+
+        private void SetLanguageDictionary()
+        {
+            switch (    Thread.CurrentThread.CurrentCulture.ToString())
+            {       
+                default:
+                    CommandRunner.Resources.Culture = new System.Globalization.CultureInfo("en-US");
+                    break;
+            }
+        }
+    }
+
+
+}
