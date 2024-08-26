db677e2f4c81a494ca10c9cdcb84e06db264f846
Adam Samec <asamec@microsoft.com>
2024-08-26 14:29:23 +0200

    Allow expand ComboBox with Down arrow key

---
+++ CommandRunner/DownArrowDropdownComboBox.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CommandRunner
+{
+    /// <summary>
+    /// ComboBox with Down arrow key behavior modification
+    /// </summary>
+    public class DownArrowDropdownComboBox : ComboBox
+    {
+        public DownArrowDropdownComboBox() { }
+
+        protected override void OnPreviewKeyDown(KeyEventArgs e)
+        {
+            base.OnPreviewKeyDown(e);
+            if (e.Key == Key.Down && !IsDropDownOpen)
+            {
+                IsDropDownOpen = true;
+            }
+        }
+    }
+}
