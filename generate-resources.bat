21d8c18378ed412075bc2462a9ebad8155c5a8cc
Adam Samec <asamec@microsoft.com>
2024-09-01 15:45:23 +0200

    Fix and improve resources generating batch file

--- generate-resources.bat
+++ generate-resources.bat
@@ -1,2 +1,5 @@
-"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\ResGen.exe" Resources\Resources.resx /str:cs,CommandRunner /publicclass
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\ResGen.exe" CommandRunner\Resources\Resources.resx /str:cs,CommandRunner /publicclass
+del CommandRunner\Resources\Resources.resources
+title Resources generated
 pause
+
